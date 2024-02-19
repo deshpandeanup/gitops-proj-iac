3b0a891d293f17c0f52d282cedff49e5faedb40b
deshpandeanup <deshpandeanup@outlook.com>
2024-02-19 12:28:08 +0530

    Made changes according to my variables & values in AWS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "ap-south-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "vprofile-eks"
+  default     = "gitops-proj-eks"
 }
 
 ##
